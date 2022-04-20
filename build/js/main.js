2f8017522685a360b3c128c5898d3991611755b2
academy-glo <shevandina_ea@mail.ru>
2022-04-20 14:13:36 +0300

    123

--- build/js/main.js
+++ build/js/main.js
@@ -103,14 +103,33 @@ $(document).ready(function () {
 
             }
          },
+         {
+            breakpoint: 1201,
+            settings: {
+               slidesToShow: 4,
+               slidesToScroll: 4,
+
+            }
+         },
+         {
+            breakpoint: 841,
+            settings: {
+               slidesToShow: 3,
+               slidesToScroll: 3,
+
+            }
+         },
+         {
+            breakpoint: 551,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2,
+
+            }
+         },
       ]
    });
   
-   $('.article-slider__box').slick({
-      prevArrow: 'button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg"></button>',
-      nextArrow: 'button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg"></button>',
-      
-      
-   });
+   
    
 });
